ec06d4ab1e985d46d4fcddf71515fa23c43125fd
Abdin, Fadi <fabdin@express.com>
2016-07-15 14:40:21 -0400

    setup a bootstrap modal

--- public_html/js/app.js
+++ public_html/js/app.js
@@ -1,6 +1,6 @@
 'use strict';
 
-angular.module('cafe', ['ngRoute', 'controllers']).
+angular.module('cafe', ['ngRoute', 'controllers','ngAnimate','ui.bootstrap']).
         config(['$locationProvider', '$routeProvider', function ($locationProvider, $routeProvider) {
 
                 $routeProvider.when('/', {
